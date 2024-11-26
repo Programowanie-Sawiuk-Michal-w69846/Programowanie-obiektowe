79c0c3c9cdd78abf501d5f4054b9ed787ecb2a07
Programowanie-Sawiuk-Michal-w69846 <166233111+Programowanie-Sawiuk-Michal-w69846@users.noreply.github.com>
2024-11-26 22:57:17 +0100

    Add files via upload

---
+++ Lab_1/Zad 7/Zad 7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę elementów do posortowania: ");
+        int n = int.Parse(Console.ReadLine());
+        double[] liczby = new double[n];
+
+        Console.WriteLine("Wprowadź liczby:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Liczba {i + 1}: ");
+            liczby[i] = double.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (liczby[j] > liczby[j + 1])
+                {
+                    double temp = liczby[j];
+                    liczby[j] = liczby[j + 1];
+                    liczby[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("Posortowane liczby:");
+        foreach (double liczba in liczby) Console.WriteLine(liczba);
+    }
+}
