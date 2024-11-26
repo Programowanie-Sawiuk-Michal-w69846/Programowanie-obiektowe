a9ed43d04fd691c4ff1d639d63568b2e51e1e710
Programowanie-Sawiuk-Michal-w69846 <166233111+Programowanie-Sawiuk-Michal-w69846@users.noreply.github.com>
2024-11-26 22:54:42 +0100

    Add files via upload

---
+++ Lab_1/Zad 2/Zad 2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\nKalkulator");
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Różnica");
+            Console.WriteLine("3. Iloczyn");
+            Console.WriteLine("4. Iloraz");
+            Console.WriteLine("5. Potęga");
+            Console.WriteLine("6. Pierwiastek");
+            Console.WriteLine("7. Funkcje trygonometryczne");
+            Console.WriteLine("8. Wyjście");
+            Console.Write("Wybierz opcję: ");
+            int wybor = int.Parse(Console.ReadLine());
+
+            if (wybor == 8) break;
+
+            if (wybor >= 1 && wybor <= 5)
+            {
+                Console.Write("Podaj pierwszą liczbę: ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("Podaj drugą liczbę: ");
+                double b = double.Parse(Console.ReadLine());
+
+                switch (wybor)
+                {
+                    case 1: Console.WriteLine($"Suma = {a + b}"); break;
+                    case 2: Console.WriteLine($"Różnica = {a - b}"); break;
+                    case 3: Console.WriteLine($"Iloczyn = {a * b}"); break;
+                    case 4:
+                        if (b != 0) Console.WriteLine($"Iloraz = {a / b}");
+                        else Console.WriteLine("Nie dziel przez 0!");
+                        break;
+                    case 5: Console.WriteLine($"Potęga = {Math.Pow(a, b)}"); break;
+                }
+            }
+            else if (wybor == 6)
+            {
+                Console.Write("Podaj liczbę: ");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Pierwiastek = {Math.Sqrt(a)}");
+            }
+            else if (wybor == 7)
+            {
+                Console.Write("Podaj kąt w radianach: ");
+                double radiany = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Sin = {Math.Sin(radiany)}, Cos = {Math.Cos(radiany)}, Tan = {Math.Tan(radiany)}");
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowy wybór.");
+            }
+        }
+    }
+}
---
+++ Lab_1/Zad 3/Zad 3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] tablica = new double[10];
+        Console.WriteLine("Wprowadź 10 liczb:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Liczba {i + 1}: ");
+            tablica[i] = double.Parse(Console.ReadLine());
+        }
+
+        do
+        {
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Wyświetl od pierwszego do ostatniego");
+            Console.WriteLine("2. Wyświetl od ostatniego do pierwszego");
+            Console.WriteLine("3. Elementy o nieparzystych indeksach");
+            Console.WriteLine("4. Elementy o parzystych indeksach");
+            Console.WriteLine("5. Wyjście");
+            Console.Write("Wybierz opcję: ");
+            int wybor = int.Parse(Console.ReadLine());
+
+            if (wybor == 5) break;
+
+            if (wybor == 1)
+            {
+                for (int i = 0; i < 10; i++) Console.Write(tablica[i] + " ");
+                Console.WriteLine();
+            }
+            else if (wybor == 2)
+            {
+                for (int i = 9; i >= 0; i--) Console.Write(tablica[i] + " ");
+                Console.WriteLine();
+            }
+            else if (wybor == 3)
+            {
+                for (int i = 1; i < 10; i += 2) Console.Write(tablica[i] + " ");
+                Console.WriteLine();
+            }
+            else if (wybor == 4)
+            {
+                for (int i = 0; i < 10; i += 2) Console.Write(tablica[i] + " ");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowy wybór.");
+            }
+        } while (true);
+    }
+}
