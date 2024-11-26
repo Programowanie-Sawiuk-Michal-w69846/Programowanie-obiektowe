2d77600d2fbe4bf0f29d79a0cb628887597be5df
Programowanie-Sawiuk-Michal-w69846 <166233111+Programowanie-Sawiuk-Michal-w69846@users.noreply.github.com>
2024-11-26 22:56:03 +0100

    Add files via upload

---
+++ Lab_1/Zad 4/Zad 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] liczby = new double[10];
+        Console.WriteLine("Wprowadź 10 liczb:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Liczba {i + 1}: ");
+            liczby[i] = double.Parse(Console.ReadLine());
+        }
+
+        double suma = 0, iloczyn = 1, min = liczby[0], max = liczby[0];
+        foreach (double liczba in liczby)
+        {
+            suma += liczba;
+            iloczyn *= liczba;
+            if (liczba < min) min = liczba;
+            if (liczba > max) max = liczba;
+        }
+
+        Console.WriteLine($"Suma: {suma}");
+        Console.WriteLine($"Iloczyn: {iloczyn}");
+        Console.WriteLine($"Średnia: {suma / liczby.Length}");
+        Console.WriteLine($"Minimalna: {min}");
+        Console.WriteLine($"Maksymalna: {max}");
+    }
+}
---
+++ Lab_1/Zad 6/Zad 6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Podaj liczbę całkowitą: ");
+            int liczba = int.Parse(Console.ReadLine());
+            if (liczba < 0)
+            {
+                Console.WriteLine("Koniec programu.");
+                break;
+            }
+        }
+    }
+}
